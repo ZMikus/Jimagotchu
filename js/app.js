eead6c05e407dd6aafe62931d82fc1427186e786
Zach Mikus <zachmikus@Zachs-MacBook-Pro.local>
2019-04-03 11:09:13 -0500

    CSS & Logic Added

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ const game = {
 	timer: 0,
 	currentPet: null,
 	gameOn: false,
+	lights: false,
 	startTimer() {
 		if (this.gameOn === false) {
 			const pet1 = new Tamagotchi()
@@ -32,14 +33,17 @@ const game = {
 				$('#sleepiness').text('Sleepiness: ' + game.currentPet.sleepiness)
 
 				game.increaseAge()
-				$('#age').text('Age: ' + game.currentPet.age)
+				$('#age').text('Weeks: ' + game.currentPet.age)
 
-				$('#timer').text('Timer: ' + game.timer)
+				$('#timer').text('Days You Kept Up: ' + game.timer)
 
 				game.gameOver()
 
+				game.youWin()
+
 				game.timer++
 			}, 1000)
+			
 		}
 
 	},
@@ -49,7 +53,8 @@ const game = {
 		}
 	},
 	decreaseBoredom(){
-		this.currentPet.boredom -=2;
+		if(this.currentPet.boredom >= 0){
+		this.currentPet.boredom -=2;}
 	},
 
 	increaseHunger(){
@@ -58,6 +63,7 @@ const game = {
 		}
 	},
 	decreaseHunger(){
+		if(this.currentPet.hunger >= 0)
 		this.currentPet.hunger -=2;
 	},
 	increaseSleepiness(){
@@ -66,41 +72,67 @@ const game = {
 		}
 	},
 	decreaseSleepiness(){
+		if(this.currentPet.sleepiness >= 0)
 		this.currentPet.sleepiness -=3;
 		},
 	increaseAge(){
-		if((this.timer > 0) && (this.timer % 15 === 0)){
+		if((this.timer > 0) && (this.timer % 7 === 0)){
 			this.currentPet.age += 1;
 		}
 	},
 	gameOver(){
 		if(this.currentPet.boredom >= 10 || 
 			this.currentPet.hunger >= 10 || 
-			this.currentPet.sleepiness >= 10 ||
-			this.currentPet.age > 10){
-			this.currentPet = null;
+			this.currentPet.sleepiness >= 10){
+			//this.currentPet.age > 11){
+			this.currentPet = null
+			$('h1').text("You Didn't Keep Up..You Fail..").css("color", "red");
 		}
-	}
+
+	},
+	
+	youWin(){
+		if(this.currentPet.age > 12) {
+			this.currentPet = null
+			$('h1').text("YOU PASSED!!").css("color", "green");
+
+		}
+	},
 }
+
+
+
+	// lightsOut(){
+	
+	// 	console.log(this.lightsOut);
+	// 	}
+	
 
 //---------------LISTENERS
 
 $('#begin-btn').on('click', () => {
 	const tamaName = $('#input-box').val();
-	$('h2').text(`${tamaName} is ALIVE! Keep it that way!!`); 
+	$('h1').text(`Keep Up Wit ${tamaName}!!`); 
 	game.startTimer()
 });
 
+
 $('#play-btn').on('click', () => {
 	game.decreaseBoredom()
-})
+});
 
 $('#feed-btn').on('click', () => {
 	game.decreaseHunger()
-})
+});
+	
 
 $('#sleep-btn').on('click', () => {
+	if(game.lights = true)
 	game.decreaseSleepiness()
-})
+			
+	});
+$('#lights-out').on('click', game.lights = true)
+
+
 
 
